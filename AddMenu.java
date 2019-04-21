203248839b3e3d04539d0f105661fde8352927f6
gdmst <gdmst@users.noreply.github.com>
2019-04-21 13:41:56 +0700

    mmm

--- AddMenu.java
+++ AddMenu.java
@@ -2,12 +2,16 @@ import java.awt.*;
 import javax.swing.*;
 class AddMenu extends JFrame{
     public static void main(String args[]) {
-        AddMenu f = new AddMenu();
-        f.setVisible(true);
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                AddMenu f = new AddMenu();
+                f.setVisible(true);
+            }
+        });
     }
     public AddMenu(){
         setBounds(200, 300, 500, 400);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Add Menu Food");
     }
 }
--- Home.java
+++ Home.java
@@ -1,35 +1,55 @@
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-public class Home extends JFrame{ 
+public class Home extends JFrame implements ActionListener{ 
     JButton menuBtn, orderBtn, vorderBtn, payBtn, conBtn, addMenuBtn;
-    public static void main(String args[]){
-        EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                Home f = new Home();
-                f.setVisible(true);
-            }
-        });
-    }
+
 
     public Home() {
         setBounds(200, 300, 500, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Home");
 
-        MenuTop allMenu = new MenuTop();
-        setJMenuBar(allMenu);
+        /*MenuTop allMenu = new MenuTop();
+        setJMenuBar(allMenu);*/
 
-        JPanel MenuHome = new Jpanel(new GridLayout(3, 2));
+        JPanel MenuHome = new JPanel(new GridLayout(3, 2));
         add(MenuHome);
         //add all btn of the menu to the other windows
         menuBtn = new JButton("All Menu");
+        orderBtn = new JButton("Take Order");
+        vorderBtn = new JButton("View All Order");
+        payBtn = new JButton("Payment");
+        conBtn = new JButton("Conclude");
+        addMenuBtn = new JButton("Add Menu");
         MenuHome.add(menuBtn);
-        orderBtn = new JButton("All Menu");
         MenuHome.add(orderBtn);
-        menuBtn = new JButton("All Menu");
-        MenuHome.add(menuBtn);
+        MenuHome.add(vorderBtn);
+        MenuHome.add(payBtn);
+        MenuHome.add(conBtn);
+        MenuHome.add(addMenuBtn);
+        menuBtn.addActionListener(this);
+        orderBtn.addActionListener(this);
+        vorderBtn.addActionListener(this);
+        payBtn.addActionListener(this);
+        conBtn.addActionListener(this);
+        addMenuBtn.addActionListener(this);
     }
-
+    
+    public void actionPerformed(ActionEvent ev) {
+        if (ev.getSource() == menuBtn) {
+            AddMenu a = new AddMenu();
+            a.setVisible(true);
+        }
+    }
+    public static void main(String args[]){
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                Home f = new Home();
+                f.setVisible(true);
+           }
+        });
+    }
 }
 
